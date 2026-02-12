128a8e1b8cf4f85c1f64775d526a20e41c864b2e
Ayush <sharmaayush94125@gmail.com>
2026-02-12 18:17:03 +0530

    Initail commit React Hello World

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+import reactLogo from './assets/react.svg'
+
+function App() {
+  return (
+    <div className="app-container">
+        <h1>Hello React App</h1>
+        <img src={reactLogo} alt="React Logo" width="150" />
+        <h2>Welcome to React Learning!</h2>
+        <button>Get Started</button>
+      </div>
+  )
+}
+
+export default App
